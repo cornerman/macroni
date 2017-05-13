926835594f141a0e99ee62c22789e5eb505732e3
johannes karoff <johannes@karoff.net>
2017-05-13 10:48:36 +0200

    make compiler settings test version agnostic

--- core/src/test/scala/CompilerSettingsSpec.scala
+++ core/src/test/scala/CompilerSettingsSpec.scala
@@ -2,16 +2,15 @@ import macroni.CompileSpec
 import scala.reflect.runtime.universe._
 
 class CompilerSettingsSpec extends CompileSpec {
-
   "compiler settings in test" >> {
     macroni.macros.CompilerSettings() must contain(
-      startWith("-Xplugin:") and endWith(".ivy2/cache/org.scalamacros/paradise_2.11.8/jars/paradise_2.11.8-2.1.0.jar")
+      startWith("-Xplugin:") and contain("org.scalamacros/paradise") and endWith(".jar")
     )
   }
 
   "compiler settings in tree" >> {
     q"macroni.macros.CompilerSettings()" must compile.to(containTree(
-      startWith("\"-Xplugin:") and endWith(".ivy2/cache/org.scalamacros/paradise_2.11.8/jars/paradise_2.11.8-2.1.0.jar\"")
+      startWith("\"-Xplugin:") and contain("org.scalamacros/paradise") and endWith(".jar\"")
     ))
   }
 }
