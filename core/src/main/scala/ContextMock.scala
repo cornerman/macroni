639fe4d8e258eff076a8fbf141e86e710043b79c
johannes karoff <johannes@karoff.net>
2017-05-13 10:50:14 +0200

    use compile matchers with tree from contextmock

--- core/src/main/scala/ContextMock.scala
+++ core/src/main/scala/ContextMock.scala
@@ -1,9 +1,12 @@
 package macroni
 
 import org.specs2.mock.Mockito
+import org.specs2.matcher.Matcher
 import scala.reflect.{ClassTag, runtime}
 import scala.reflect.macros.Universe
 import scala.reflect.macros.blackbox
+import macroni.compiler.CompileResult
+import macroni.matcher.CompilingTreeMatcher
 
 trait ContextMock extends Mockito {
   def mockContext[C <: blackbox.Context : ClassTag]: C = {
@@ -13,6 +16,7 @@ trait ContextMock extends Mockito {
   }
 
   implicit def MacroToRuntimeTree(tree: Universe#Tree): runtime.universe.Tree = tree.asInstanceOf[runtime.universe.Tree]
+  implicit def MatcherToCompilingUniverseTreeMatcher(matcher: Matcher[CompileResult]): Matcher[Universe#Tree] = new CompilingTreeMatcher(matcher).asInstanceOf[Matcher[Universe#Tree]]
 }
 
 object ContextMock extends ContextMock
--- core/src/test/scala/ContextMockSpec.scala
+++ core/src/test/scala/ContextMockSpec.scala
@@ -1,11 +1,11 @@
-import macroni.{ContextMock, TreeSpec}
+import macroni.{ContextMock, TreeSpec, CompileSpec}
 import macroni.matcher._
 
 import org.specs2.mock.Mockito
 import scala.reflect.macros.{blackbox, whitebox}
 import org.specs2.matcher._
 
-class ContextMockSpec extends TreeSpec with ContextMock {
+class ContextMockSpec extends TreeSpec with ContextMock with CompileSpec {
   "blackbox context tree equals same tree" >> {
     val context = mockContext[blackbox.Context]
     import context.universe._
@@ -20,5 +20,12 @@ class ContextMockSpec extends TreeSpec with ContextMock {
 
     val tree = q"val golum = true"
     tree should beEqualToTree(q"val golum = true")
+  }
+
+  "mocked context with compile matcher" >> {
+    val context = mockContext[whitebox.Context]
+    import context.universe._
+
+    q"1" must compile
   }
 }
